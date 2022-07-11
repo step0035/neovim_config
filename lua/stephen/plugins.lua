db166929fb407b8df7aa7b1d82411cb54a2f1c6b
step0035 <step0035@e.ntu.edu.sg>
2022-07-12 05:29:56 +0800

    [Gitsigns] Add gitsigns

--- lua/stephen/plugins.lua
+++ lua/stephen/plugins.lua
@@ -80,6 +80,9 @@ return packer.startup(function(use)
   }
   use 'JoosepAlviste/nvim-ts-context-commentstring'
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
   -- Vimwiki
   use {
     'vimwiki/vimwiki',
@@ -96,7 +99,6 @@ return packer.startup(function(use)
         ['.markdown'] = 'markdown',
         ['.mdown'] = 'markdown',
       }
-      vimwiki_markdown_link_ext = 1
     end
   }
 
